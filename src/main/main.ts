cff9eb709138634847d8b854307751896e4e36eb
Alex <a.mas.bosch@gmail.com>
2018-11-11 01:09:34 +0100

    app now shows a loading image while html is loading

--- src/main/main.ts
+++ src/main/main.ts
@@ -6,6 +6,8 @@ import * as Electron from 'electron'
 const {app, Menu, BrowserWindow, ipcMain, WebContents} = require('electron');
 
 
+let loadingWindow: Electron.BrowserWindow;
+let mainWindow: Electron.BrowserWindow;
 
 const setApplicationMenu = () => {
 
@@ -21,22 +23,50 @@ if (process.env.ENV !== "production") {
 }
 
 app.on("ready", () => {
+    console.log('app ready');
     setApplicationMenu();
-    const mainWindow = new BrowserWindow({
-        width: 1000,
-        height: 600,
+    const screen = require('electron').screen;
+    debugger;
+    loadingWindow =  new BrowserWindow({
+        x: screen.getPrimaryDisplay().bounds.width/2 - 100,
+        y:screen.getPrimaryDisplay().bounds.height/2 - 100,
+        transparent:true,
+        frame:false
+    });
+    loadingWindow.show();
+    loadingWindow.loadURL(url.format({
+        pathname: path.join(__dirname,'loader.html'),
+        protocol: 'file',
+        slashes: true
+    }))
+    mainWindow = new BrowserWindow({
+        show: false,
         webPreferences: {
             nodeIntegrationInWorker: true,
             webSecurity: false,
-        }
+        },
+        fullscreenable: true
     });
-    mainWindow.setFullScreenable(true);
-    mainWindow.maximize();
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
-        protocol: "file:",
+        protocol: 'file:',
         slashes: true
     }));
+
+    mainWindow.webContents.once('did-finish-load',()=>{
+        loadingWindow.close();
+        mainWindow.show();
+    });
+
+    mainWindow.on('close',()=>{
+        //@ts-ignore
+        mainWindow = null;
+    })
+    loadingWindow.on('close',()=>{
+        console.log('closing loading window');
+        //@ts-ignore
+        loadingWindow = null;
+    })
     if (process.env.ENV === "development") {
         mainWindow.webContents.openDevTools();
     }
