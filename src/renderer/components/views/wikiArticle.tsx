096dffa9fe9b489c87d17d72d8b24bf4d1cb31b8
Alex <a.mas.bosch@gmail.com>
2022-05-15 11:44:49 +0200

    improve notifications style

--- src/renderer/components/views/wikiArticle.tsx
+++ src/renderer/components/views/wikiArticle.tsx
@@ -1,45 +1,41 @@
-import * as React from 'react';
-import { AppState } from '../../store/store';
-import { connect, MapStateToProps, MapDispatchToProps } from 'react-redux';
-import { Link } from 'react-router-dom';
-import WikiEditor from '../wikiEditor/wikiEditor';
-import defaultEditorContents from '../wikiEditor/utilities/defaultValue';
-import * as ReactMarkdown from 'react-markdown';
-import { Value } from 'slate';
-import { fsError, FsErrorActionCreator } from '../../actions/errors';
-import Header from '../header';
-import { loadArticle, LoadArticleActionCreator, Article, DeleteArticleActionCreator, deleteArticle, ArticleMetaData, SaveArticleActionCreator, saveArticle } from '../../actions/article';
-import WikiSearchBar from '../wikiSearchBar';
-import { getArticle } from '../../selectors/articles';
-import { WikiMetadata } from '../../store/reducers/wikis';
-import WikiHeader from '../wikiHeader';
-import HomeButton from '../homeButton';
 import I18String from '@axc/react-components/i18string';
-import WikiView from '../wikiView';
-import { withPrompt, PromptFunction } from '@axc/react-components/prompt';
-import { DeletePromptFunction, DeletePrompt } from '../deletePrompt';
-import { getSelectedWiki, getWikiById } from '../../selectors/wikis';
+import { withPrompt } from '@axc/react-components/prompt';
+import * as React from 'react';
+import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
+import { Link } from 'react-router-dom';
+import { Value } from 'slate';
+import { Article, ArticleMetaData, deleteArticle, DeleteArticleActionCreator, loadArticle, LoadArticleActionCreator, saveArticle, SaveArticleActionCreator } from '../../actions/article';
+import { fsError, FsErrorActionCreator } from '../../actions/errors';
+import { getArticle } from '../../selectors/articles';
+import { getWikiById } from '../../selectors/wikis';
+import { WikiMetadata } from '../../store/reducers/wikis';
+import { AppState } from '../../store/store';
+import { DeletePrompt, DeletePromptFunction } from '../deletePrompt';
+import defaultEditorContents from '../wikiEditor/utilities/defaultValue';
+import WikiEditor from '../wikiEditor/wikiEditor';
+import WikiSearchBar from '../wikiSearchBar';
+import WikiView from '../wikiView';
 const { dialog } = require('electron').remote
 
 
 
 interface DispatchProps {
-    fsError: FsErrorActionCreator,
-    loadArticle: LoadArticleActionCreator,
-    saveArticle: SaveArticleActionCreator,
-    deleteArticle: DeleteArticleActionCreator
+  fsError: FsErrorActionCreator,
+  loadArticle: LoadArticleActionCreator,
+  saveArticle: SaveArticleActionCreator,
+  deleteArticle: DeleteArticleActionCreator
 }
 
 interface PromptProps {
-    prompt: DeletePromptFunction;
+  prompt: DeletePromptFunction;
 }
-interface OwnProps extends RouteComponentProps<{ article: string,id:string}> {
+interface OwnProps extends RouteComponentProps<{ article: string, id: string }> {
 }
 
 interface ReduxProps {
-    selectedWiki: WikiMetadata,
-    article: ArticleMetaData
+  selectedWiki: WikiMetadata,
+  article: ArticleMetaData
 }
 
 
@@ -47,140 +43,140 @@ type PageProps = OwnProps & ReduxProps & DispatchProps & PromptProps;
 
 //  <ReactMarkdown source={fs.readFileSync(path.join(props.selectedWiki.path, 'home.md'), 'utf8')}/>
 export class WikiArticlePage extends React.Component<PageProps, any>{
-    constructor(props: PageProps) {
-        super(props);
-        this.state = {
-            content: Value.fromJSON(JSON.parse('{}')),
-            fileExists: true
-        }
+  constructor(props: PageProps) {
+    super(props);
+    this.state = {
+      content: Value.fromJSON(JSON.parse('{}')),
+      fileExists: true
     }
-    componentDidMount() {
+  }
+  componentDidMount() {
+    //@ts-ignore
+    this.props.loadArticle(this.props.selectedWiki, this.props.match.params.article, this.props.article.category).then((article: Article) => {
+      this.setState(() => ({
+        content: article.content ? Value.fromJSON(article.content) : defaultEditorContents,
+        fileExists: article.content ? true : false
+      }));
+      console.log('saving article with updated read time');
+      this.props.saveArticle({
+        ...article,
+        lastRead: Date.now()
+      }, this.props.selectedWiki);
+    }).catch((e: string) => {
+      console.log(e);
+      this.setState(() => ({
+        fileExists: false
+      }));
+    })
+  }
+  componentDidUpdate(prevProps: PageProps, prevState: any) {
+    if (this.props.match.params.article !== prevProps.match.params.article) {
+      //@ts-ignore
+      this.props.loadArticle(this.props.selectedWiki, this.props.match.params.article).then((article: Article) => {
+        this.setState(() => ({
+          content: article.content ? Value.fromJSON(article.content) : defaultEditorContents,
+          fileExists: article.content ? true : false
+        }));
+        console.log('saving article with updated read time');
+        this.props.saveArticle({
+          ...article,
+          lastRead: Date.now()
+        }, this.props.selectedWiki);
+      }).catch(() => {
+        this.setState(() => ({
+          fileExists: false
+        }));
+      })
+
+    }
+  }
+  deleteArticle = () => {
+    this.props.prompt(DeletePrompt, { title: 'are you sure you want to delete this article?' }).then((response) => {
+      if (response) {
         //@ts-ignore
-        this.props.loadArticle(this.props.selectedWiki,this.props.match.params.article, this.props.article.category).then((article: Article) => {
-            this.setState(() => ({
-                content: article.content ? Value.fromJSON(article.content) : defaultEditorContents,
-                fileExists: article.content ? true : false
-            }));
-            console.log('saving article with updated read time');
-            this.props.saveArticle({
-                ...article,
-                lastRead: Date.now()
-            },this.props.selectedWiki);
-        }).catch((e: string) => {
-            console.log(e);
-            this.setState(() => ({
-                fileExists: false
-            }));
-        })
-    }
-    componentDidUpdate(prevProps: PageProps, prevState: any) {
-        if (this.props.match.params.article !== prevProps.match.params.article) {
-            //@ts-ignore
-            this.props.loadArticle(this.props.selectedWiki,this.props.match.params.article).then((article: Article) => {
-                this.setState(() => ({
-                    content: article.content ? Value.fromJSON(article.content) : defaultEditorContents,
-                    fileExists: article.content ? true : false
-                }));
-                console.log('saving article with updated read time');
-                this.props.saveArticle({
-                    ...article,
-                    lastRead: Date.now()
-                }, this.props.selectedWiki);
-            }).catch(() => {
-                this.setState(() => ({
-                    fileExists: false
-                }));
-            })
-
-        }
-    }
-    deleteArticle = () => {
-        this.props.prompt(DeletePrompt, { title: 'are you sure you want to delete this article?' }).then((response) => {
-            if (response) {
-                //@ts-ignore
-                this.props.deleteArticle(this.props.match.params.article, this.props.selectedWiki).then(() => {
-                    this.props.history.push(`/wiki/${this.props.match.params.id}`);
-                });
-            }
+        this.props.deleteArticle(this.props.match.params.article, this.props.selectedWiki).then(() => {
+          this.props.history.push(`/wiki/${this.props.match.params.id}`);
         });
-    }
-    onChange = (change: { operations: any, value: Value }) => {
-        const content = change.value;
-        this.setState(() => ({ content }));
-    }
-    renderArticleNotFound = () => {
-        return (
-            <WikiView
-                title={`${this.props.selectedWiki.name}@${this.props.match.params.article}`}
-            >
-                <div className='wiki-article'>
-                    <div className='wiki-article__header'>
-                        <div className='wiki-article__header__section'>
-                            <h1 className='wiki-article__title'> <I18String text='article not found' format='capitalizeFirst' /></h1>
-                            <div className='wiki-article__actions'>
-                                <Link to={`/wiki/${this.props.match.params.id}/create/${this.props.match.params.article}`}><i className="material-icons">add</i></Link>
-                            </div>
-                        </div>
-                    </div>
+      }
+    });
+  }
+  onChange = (change: { operations: any, value: Value }) => {
+    const content = change.value;
+    this.setState(() => ({ content }));
+  }
+  renderArticleNotFound = () => {
+    return (
+      <WikiView
+        title={`${this.props.selectedWiki.name}@${this.props.match.params.article}`}
+      >
+        <div className='wiki-article'>
+          <div className='wiki-article__header'>
+            <div className='wiki-article__header__section'>
+              <h1 className='wiki-article__title'> <I18String text='article not found' format='capitalizeFirst' /></h1>
+              <div className='wiki-article__actions'>
+                <Link to={`/wiki/${this.props.match.params.id}/create/${this.props.match.params.article}`}><i className="material-icons">add</i></Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </WikiView>
+    )
+  }
+
+  render() {
+    const article = this.props.match.params.article;
+    if (this.state.fileExists) {
+      return (
+        <WikiView
+          title={`${this.props.selectedWiki.name}@${this.props.match.params.article}`}
+        >
+          <div className='wiki-article'>
+            <div className='wiki-article__header'>
+              <div className='wiki-article__searchbar'>
+                <WikiSearchBar />
+              </div>
+              <div className='wiki-article__header__section'>
+                <h1 className='wiki-article__title'>{article === 'home' ? this.props.selectedWiki.name : article}</h1>
+                <div className='wiki-article__actions'>
+                  {article !== 'home' ? <button onClick={this.deleteArticle}><i className='material-icons'>delete_forever</i></button> : null}
+                  <Link to={`/wiki/${this.props.match.params.id}/edit/${article}`}><i className='material-icons'>create</i></Link>
                 </div>
-            </WikiView>
-        )
+              </div>
+            </div>
+            <div className='wiki-article__body'>
+              <WikiEditor
+                content={this.state.content}
+                onChange={this.onChange}
+                readOnly={true}
+              />
+            </div>
+          </div>
+        </WikiView>
+      )
+    } else {
+      return this.renderArticleNotFound();
     }
 
-    render() {
-        const article = this.props.match.params.article;
-        if (this.state.fileExists) {
-            return (
-                <WikiView
-                    title={`${this.props.selectedWiki.name}@${this.props.match.params.article}`}
-                >
-                    <div className='wiki-article'>
-                        <div className='wiki-article__header'>
-                            <div className='wiki-article__searchbar'>
-                                <WikiSearchBar />
-                            </div>
-                            <div className='wiki-article__header__section'>
-                                <h1 className='wiki-article__title'>{article === 'home' ? this.props.selectedWiki.name : article}</h1>
-                                <div className='wiki-article__actions'>
-                                    {article !== 'home' ? <button onClick={this.deleteArticle}><i className='material-icons'>delete_forever</i></button> : null}
-                                    <Link to={`/wiki/${this.props.match.params.id}/edit/${article}`}><i className='material-icons'>create</i></Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='wiki-article__body'>
-                            <WikiEditor
-                                content={this.state.content}
-                                onChange={this.onChange}
-                                readOnly={true}
-                            />
-                        </div>
-                    </div>
-                </WikiView>
-            )
-        } else {
-            return this.renderArticleNotFound();
-        }
-
-    }
+  }
 }
 
 
 
 
-
+//@ts-ignore
 export default withPrompt<PageProps>(connect(
-    (state: AppState, props: OwnProps) => {
-        const selectedWiki = getWikiById(state, props.match.params.id);
-        return {
-            selectedWiki,
-            article: getArticle(props.match.params.article, selectedWiki)
-        };
-    },
-    {
-        fsError,
-        loadArticle,
-        deleteArticle,
-        saveArticle
-    }
-    //@ts-ignore
+  (state: AppState, props: OwnProps) => {
+    const selectedWiki = getWikiById(state, props.match.params.id);
+    return {
+      selectedWiki,
+      article: getArticle(props.match.params.article, selectedWiki)
+    };
+  },
+  {
+    fsError,
+    loadArticle,
+    deleteArticle,
+    saveArticle
+  }
+  //@ts-ignore
 )(WikiArticlePage));
